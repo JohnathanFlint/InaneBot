0da0b0973354161145fc4ecf0884756cbaf13ba0
JohnathanFlint <nicholas.white142@gmail.com>
2016-11-03 09:54:51 -0600

    Started setting up the frame and panel classes.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,25 @@
 package chat.view;
 
-public class ChatFrame
-{
+import javax.swing.JFrame;
+import chat.controller.ChatbotController;
+import java.awt.Dimension;
 
+public class ChatFrame extends JFrame
+{
+	private ChatbotController baseController;
+	private ChatPanel appPanel;
+	
+	public ChatFrame (ChatbotController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		appPanel = new ChatPanel(baseController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setVisible(true);
+	}
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,39 @@
 package chat.view;
 
-public class ChatPanel
-{
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController baseController;
+	
+	public ChatPanel(ChatbotController baseController)
+	{
+		this.baseController = baseController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
